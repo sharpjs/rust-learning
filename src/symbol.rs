26b58ce82e6672bf595440a2cd39071448566d32
Jeff Sharp <jeff.sharp@git>
2015-09-19 16:17:37 -0500

    Basic symbol table, after much frustration.

--- src/symbol.rs
+++ src/symbol.rs
@@ -1,34 +1,61 @@
 use std::collections::HashMap;
 
-struct StringTable<'a> {
-    map: HashMap<&'a str, &'a str>
+#[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct SymbolId(usize);
+
+#[derive(Clone, Debug)]
+pub struct Symbol {
+    pub name: String,
+    // other fields here
 }
 
-impl<'a> StringTable<'a> {
-    fn new() -> StringTable<'a> {
-        StringTable { map: HashMap::new() }
+pub struct SymbolTable {
+    map: HashMap<String, SymbolId>,
+    vec: Vec<Box<Symbol>>
+}
+
+impl SymbolTable {
+    fn new() -> SymbolTable {
+        SymbolTable {
+            map: HashMap::new(),
+            vec: Vec    ::new()
+        }
     }
 
-    fn intern(&mut self, s: &'a str) -> &'a str {
-        let map = &mut self.map;
-        if let Some(s) = map.get(&s) {
-            return *s;
+    fn intern<S>(&mut self, name: S) -> SymbolId
+        where S: Into<String>
+    {
+        let name = name.into();
+
+        // If a symbol exists by that name, return it
+        if let Some(&idx) = self.map.get(&name) {
+            return idx;
         }
-        map.insert(s, s);
-        s
+
+        // Else, build a new symbol and return it
+        let id  = SymbolId(self.vec.len());
+        let key = name.clone();
+        let sym = Box::new(Symbol {
+            name: name,
+            // other fields here
+        });
+        self.vec.push(sym);
+        self.map.insert(key, id);
+        id
+    }
+
+    fn get(&self, id: SymbolId) -> &Symbol {
+        &*self.vec[id.0]
     }
 }
 
 #[test]
 fn test() {
-    let a = "hello";
-    let b = String::from("h") + "ello";
+    let mut t = SymbolTable::new();
 
-    let mut t = StringTable::new();
+    let a = t.intern("Hello");
+    let b = t.intern("Hello");
 
-    //assert!("a".as_ptr() == "b".as_ptr());
-
-    assert_eq!(t.intern( a).as_ptr(), a.as_ptr());
-    assert_eq!(t.intern(&b).as_ptr(), a.as_ptr());
+    assert_eq!(a, b);
 }
 
