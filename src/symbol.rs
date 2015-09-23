45afdfbd5bb0f3bc3751b87e157f90a5a9674e05
Jeff Sharp <jeff.sharp@git>
2015-09-23 07:50:09 -0500

    Rename symbol structs; use Borrow trait.

--- src/symbol.rs
+++ src/symbol.rs
@@ -1,30 +1,32 @@
+use std::borrow::Borrow;
 use std::collections::HashMap;
 
 #[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
-pub struct SymbolId(usize);
+pub struct Symbol(usize);
 
 #[derive(Clone, Debug)]
-pub struct Symbol {
+pub struct SymbolInfo {
     pub name: String,
     // other fields here
 }
 
 pub struct SymbolTable {
-    map: HashMap<String, SymbolId>,
-    vec: Vec<Box<Symbol>>
+    map: HashMap<String, Symbol>,
+    vec: Vec<Box<SymbolInfo>>
 }
 
 impl SymbolTable {
-    fn new() -> SymbolTable {
+    fn new() -> Self {
         SymbolTable {
             map: HashMap::new(),
             vec: Vec    ::new()
         }
     }
 
-    fn intern<S>(&mut self, name: S) -> SymbolId
+    fn intern<S>(&mut self, name: S) -> Symbol
         where S: Into<String>
     {
+        // We own the name, thank you
         let name = name.into();
 
         // If a symbol exists by that name, return it
@@ -33,9 +35,9 @@ impl SymbolTable {
         }
 
         // Else, build a new symbol and return it
-        let id  = SymbolId(self.vec.len());
+        let id  = Symbol(self.vec.len());
         let key = name.clone();
-        let sym = Box::new(Symbol {
+        let sym = Box::new(SymbolInfo {
             name: name,
             // other fields here
         });
@@ -44,8 +46,8 @@ impl SymbolTable {
         id
     }
 
-    fn get(&self, id: SymbolId) -> &Symbol {
-        &*self.vec[id.0]
+    fn get(&self, id: Symbol) -> &SymbolInfo {
+        self.vec[id.0].borrow()
     }
 }
 
@@ -54,7 +56,7 @@ fn test() {
     let mut t = SymbolTable::new();
 
     let a = t.intern("Hello");
-    let b = t.intern("Hello");
+    let b = t.intern("Hello".to_string());
 
     assert_eq!(a, b);
 }
