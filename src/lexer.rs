c79d52ee3cb1f6188ae8c815b8c4dc7b6937f6cf
Jeff Sharp <jeff.sharp@git>
2015-09-27 17:45:36 -0500

    Integer literal tests passing, but still WIP.

--- src/lexer.rs
+++ src/lexer.rs
@@ -115,72 +115,114 @@ where I: Iterator<Item=char>
         Some(Id(sym))
     }
 
-    // Scan number (at 0)
+    // Scan number (at initial 0)
     //
     fn lex_num_zero(&mut self) -> Option<Token> {
-        let input = self.start().advance().ch();
-        match input {
+        match self.start().advance().ch() {
             Some(c) => match c {
-                '0'...'9' => { self          .lex_num_radix(10) },
-                'x'       => { self.advance().lex_num_radix(16) },
-                'o'       => { self.advance().lex_num_radix( 8) },
-                'b'       => { self.advance().lex_num_radix( 2) },
-                 _        => { self          .lex_after_num(c, 0) }
+                '0'...'9' => { self          .lex_num_zero_digit (c)     },
+                '_'       => { self.advance().lex_num_rest       (10, 0) },
+                'x'       => { self.advance().lex_num_radix      (16   ) },
+                'o'       => { self.advance().lex_num_radix      ( 8   ) },
+                'b'       => { self.advance().lex_num_radix      ( 2   ) },
+                 _        => { self          .lex_after_num      ( c, 0) }
             },
             None => Some(Int(0))
         }
     }
 
-    // Scan number (at 1-9)
+    // Scan number (at initial 1-9)
     //
     #[inline]
     fn lex_num_nonzero(&mut self) -> Option<Token> {
-        self.start().lex_num_radix(10)
+        let val = self.ch().unwrap().to_digit(10).unwrap();
+        self.start().advance().lex_num_rest(10, val)
     }
 
-    // Scan number (at fist digit of known radix)
+    // Scan number (after initial 0, at digit)
+    //
+    #[inline]
+    fn lex_num_zero_digit(&mut self, c: char) -> Option<Token> {
+        self.lex_num_radix_digit(10, c.to_digit(10).unwrap())
+    }
+
+    // Scan number (at first char after any radix prefix)
     //
     fn lex_num_radix(&mut self, radix: u32) -> Option<Token> {
         let mut input = self.ch();
-        let mut value: u32;
 
-        // Allow leading _, but require a digit
-        loop {
-            match input {
-                Some('_') => {
-                    input = self.advance().ch();
-                    continue;
-                },
-                Some(c) => {
-                    if let Some(d) = c.to_digit(radix) {
-                        input = self.advance().ch();
-                        value = d;
-                        break;
-                    }
-                },
-                _ => {}
-            }
-            return self.err_int_format();
+        // Ignore leading _
+        while let Some('_') = input {
+            input = self.advance().ch();
         }
+
+        // Require a digit
+        if let Some(c) = input {
+            if let Some(d) = c.to_digit(radix) {
+                return self.advance().lex_num_rest(radix, d)
+            }
+        }
+
+        // Eof doesn't make sense here
+        self.err_int_format()
+    }
+
+    // Scan number (at first digit of known radix)
+    //
+    fn lex_num_radix_digit(&mut self, radix: u32, mut value: u32) -> Option<Token> {
+        loop {
+            let input = self.advance().ch();
+            match input {
+                Some('_') => {},
+                Some( c ) => match c.to_digit(radix) {
+                    Some(d) => value = value * radix + d,
+                    _       => return self.lex_after_num(c, value)
+                },
+                _ => return Some(Int(value))
+            }
+        }
+    }
+
+    // Scan number (after first digit of known radix)
+    //
+    fn lex_num_rest(&mut self, radix: u32, mut value: u32) -> Option<Token> {
+        let mut input = self.ch();
 
         // Match digits or _
         loop {
+        //    while let Some('_') = input {
+        //        input = self.advance().ch();
+        //    }
+
+        //    if let Some(c) = input {
+        //        if let Some(d) = c.to_digit(radix) {
+        //            input = self.advance().ch();
+        //            value = value * radix + d;
+        //            continue;
+        //        } else {
+        //            return self.lex_after_num(c, value);
+        //        }
+        //    }
+
+        //    // Eof is OK here
+        //    return Some(Int(value))
+        //}
+
             match input {
                 Some('_') => {
                     input = self.advance().ch();
-                    continue;
                 },
                 Some(c) => {
-                    if let Some(d) = c.to_digit(radix) {
-                        input = self.advance().ch();
-                        value = value * radix + d;
-                        continue;
+                    match c.to_digit(radix) {
+                        Some(d) => {
+                            input = self.advance().ch();
+                            value = value * radix + d;
+                        },
+                        _ => return self.lex_after_num(c, value)
                     }
-                    return self.lex_after_num(c, value);
                 },
-                _ => {}
+                None => return Some(Int(value))
             }
-            return Some(Int(value));
         }
     }
 
@@ -257,45 +299,6 @@ where I: Iterator<Item=char>
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use super::Token::*;
-
-    #[test]
-    fn num() {
-        assert_eq!(lex( "123456789"), Int( 123456789));
-        assert_eq!(lex("0x01234567"), Int(0x01234567));
-        assert_eq!(lex("0x89ABCDEF"), Int(0x89ABCDEF));
-        assert_eq!(lex("0x89abcdef"), Int(0x89ABCDEF));
-        assert_eq!(lex("0o01234567"), Int(0o01234567));
-        assert_eq!(lex(      "0b01"), Int(      0b01));
-        assert_eq!(lex(       "012"), Int(        12));
-        assert_eq!(lex(         "0"), Int(         0));
-        assert_eq!(lex(    "1__2__"), Int(        12));
-        assert_eq!(lex("0x__1__2__"), Int(      0x12));
-        assert_eq!(lex(       "0__"), Int(         0));
-        assert_eq!(lex(     "0b1  "), Int(         1));
-        assert_eq!(lex(     "0b1;;"), Int(         1));
-        assert_eq!(lex(     "0b19z"), Eof            ); // TODO: error
-    }
-
-    #[test]
-    fn id() {
-        assert_eq!(lex("a"  ), Id(0.into()));
-        assert_eq!(lex("a;" ), Id(0.into()));
-        assert_eq!(lex("_a1"), Id(0.into()));
-    }
-
-    fn lex(s: &str) -> Token {
-        let mut lexer = Lexer::new(s.chars());
-        let     token = lexer.lex();
-        assert_eq!(lexer.errs, 0);
-        token
-    }
-}
-
-
 impl<I> Lookahead for Lexer<I>
 where I: Iterator<Item=char>
 {
@@ -340,6 +343,80 @@ impl<I> Lookahead for IterLookahead<I> where I: Iterator, I::Item: Copy {
         let item = self.iter.next();
         self.item = item;
         item
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::Token::*;
+
+    #[test]
+    fn eof() {
+        lex("", Eof, (1, 1), (1, 1), 0);
+    }
+
+    #[test]
+    fn space() {
+        lex(" \t", Eof, (1, 3), (1, 3), 0);
+    }
+
+    #[test]
+    fn eos() {
+        lex(  "\n", Eos, (1, 1), (2, 1), 0);
+        lex("\r\n", Eos, (1, 1), (2, 1), 0);
+        lex("\r"  , Eos, (1, 1), (2, 1), 0);
+    }
+
+    #[test]
+    fn id() {
+        lex_id("a"  , "a",   (1, 1), (1, 2), 0);
+        lex_id("a;" , "a",   (1, 1), (1, 2), 0);
+        lex_id("_a1", "_a1", (1, 1), (1, 4), 0);
+    }
+
+    #[test]
+    fn num() {
+        lex( "123456789", Int( 123456789), (1, 1), (1, 10), 0);
+        lex("0x01234567", Int(0x01234567), (1, 1), (1, 11), 0);
+        lex("0x89ABCDEF", Int(0x89ABCDEF), (1, 1), (1, 11), 0);
+        lex("0x89abcdef", Int(0x89ABCDEF), (1, 1), (1, 11), 0);
+        lex("0o01234567", Int(0o01234567), (1, 1), (1, 11), 0);
+        lex(      "0b01", Int(      0b01), (1, 1), (1,  5), 0);
+        lex(       "012", Int(        12), (1, 1), (1,  4), 0);
+        lex(         "0", Int(         0), (1, 1), (1,  2), 0);
+        lex(    "1__2__", Int(        12), (1, 1), (1,  7), 0);
+        lex("0x__1__2__", Int(      0x12), (1, 1), (1, 11), 0);
+        lex(       "0__", Int(         0), (1, 1), (1,  4), 0);
+        lex(     "0b1  ", Int(         1), (1, 1), (1,  4), 0);
+        lex(     "0b1;;", Int(         1), (1, 1), (1,  4), 0);
+        lex(     "0b19z", Eof            , (1, 6), (1,  6), 0); // TODO: error
+    }
+
+    fn lex(s: &str, t: Token, start: (u32, u32), end: (u32, u32), errs: u32)
+    {
+        let mut lexer = Lexer::new(s.chars());
+        let     token = lexer.lex();
+
+        assert_eq!(token,       t);
+        assert_eq!(lexer.token, t);
+        assert_eq!((lexer.start.line, lexer.start.column), start);
+        assert_eq!((lexer.end  .line, lexer.end  .column), end  );
+        assert_eq!(lexer.errs, errs);
+    }
+
+    fn lex_id(s: &str, name: &str, start: (u32, u32), end: (u32, u32), errs: u32)
+    {
+        let mut lexer = Lexer::new(s.chars());
+
+        match lexer.lex() {
+            Id(sym) => assert_eq!(lexer.syms.get(sym).name, name),
+            _       => panic!("Did not return an identifier.")
+        }
+
+        assert_eq!((lexer.start.line, lexer.start.column), start);
+        assert_eq!((lexer.end  .line, lexer.end  .column), end  );
+        assert_eq!(lexer.errs, errs);
     }
 }
 
