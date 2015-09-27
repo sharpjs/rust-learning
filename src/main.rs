213db401f1cf27b2f5f016f5789c75ad62ae0615
Jeff Sharp <jeff.sharp@git>
2015-09-27 17:45:01 -0500

    is! macro, to test if a match exists.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+macro_rules! is {
+    { $val:expr => $( $pat:pat ),* } => {
+        match $val {
+            $( $pat => true ),* ,
+            _ => false
+        }
+    };
+    { $val:expr => $( $pat:pat if $cond:expr ),* } => {
+        match $val {
+            $( $pat if $cond => true ),* ,
+            _ => false
+        }
+    };
+}
+
 mod char;
 mod lexer;
 mod parser;
