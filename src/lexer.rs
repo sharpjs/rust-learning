0e8b140b8109ce63384177896c977c61527a8357
Jeff Sharp <jeff.sharp@git>
2015-10-15 06:44:10 -0500

    Cleanup. The new idea has superseded the old one.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,13 +1,6 @@
 use interner::*;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub struct Pos {
-    pub byte:   usize,  // 0-based byte offset
-    pub line:   u32,    // 1-based line number
-    pub column: u32,    // 1-based column number
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum Token {
     Id      (Symbol),
     Int     (u64),
@@ -17,13 +10,12 @@ pub enum Token {
 }
 use self::Token::*;
 
-pub trait Lookahead {
-    type Item;
-    fn current (&    self) -> Self::Item;
-    fn next    (&mut self) -> Self::Item;
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct Pos {
+    pub byte:   usize,  // 0-based byte offset
+    pub line:   u32,    // 1-based line number
+    pub column: u32,    // 1-based column number
 }
-
-// Begin new idea 2015-10-13
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 #[repr(u8)]
@@ -32,10 +24,11 @@ enum State {
     AfterZero, InNumDec, InNumHex, InNumOct, InNumBin,
     AtEof
 }
-const STATE_COUNT: usize = 9;
 use self::State::*;
 
-type StateEntry = (
+const STATE_COUNT: usize = 9;
+
+type ActionTable = (
     [u8; 128],      // Map from 7-bit char to transition index
     &'static [(     // Transitions
         State,      // - next state
@@ -46,7 +39,7 @@ type StateEntry = (
 
 type Action = Option< fn(&mut Context, char) -> Option<Token> >;
 
-pub struct Lexer2<I>
+pub struct Lexer<I>
 where I: Iterator<Item=char>
 {
     iter:       I,              // remaining chars
@@ -58,13 +51,13 @@ where I: Iterator<Item=char>
 struct Context {
     start:      Pos,            // position of token start
     current:    Pos,            // position of current character
-    buffer:     String,         // string builder
     number:     u64,            // number builder
-    strings:    Interner        // string table
+    buffer:     String,         // string builder
+    strings:    Interner        // string interner
     // errors
 }
 
-impl<I> Lexer2<I>
+impl<I> Lexer<I>
 where I: Iterator<Item=char>
 {
     fn new(mut iter: I) -> Self {
@@ -78,13 +71,14 @@ where I: Iterator<Item=char>
             strings: Interner::new()
             // errors
         };
-        Lexer2 { iter:iter, ch:ch, state:state, context:context }
+        Lexer { iter:iter, ch:ch, state:state, context:context }
     }
 
     fn lex(&mut self) -> Token {
-        let     iter  = &mut self.iter;
         let mut ch    =      self.ch;
         let mut state =      self.state;
+        let     iter  = &mut self.iter;
+        let     ctx   = &mut self.context;
 
         loop {
             let (c, (next_state, consume, action))
@@ -93,13 +87,13 @@ where I: Iterator<Item=char>
             state = next_state;
 
             if consume {
-                self.context.current.byte   += c.len_utf8();
-                self.context.current.column += 1;
+                ctx.current.byte   += c.len_utf8();
+                ctx.current.column += 1;
                 ch = iter.next();
             }
 
             if let Some(func) = action {
-                if let Some(token) = func(&mut self.context, c) {
+                if let Some(token) = func(ctx, c) {
                     // Remember state for next call
                     self.ch    = ch;
                     self.state = state;
@@ -111,7 +105,7 @@ where I: Iterator<Item=char>
 }
 
 #[inline]
-fn lookup(entry: &StateEntry, ch: Option<char>) -> (char, (State, bool, Action))
+fn lookup(entry: &ActionTable, ch: Option<char>) -> (char, (State, bool, Action))
 {
     let (n, c) = match ch {
         Some(c) => {
@@ -133,7 +127,7 @@ fn lookup(entry: &StateEntry, ch: Option<char>) -> (char, (State, bool, Action))
 #[allow(non_upper_case_globals)]
 const x: u8 = 1;
 
-static STATES: [StateEntry; STATE_COUNT] = [
+static STATES: [ActionTable; STATE_COUNT] = [
     // Initial
     ([
         x, x, x, x, x, x, x, x,  x, 2, 3, x, x, 2, x, x, // ........ .tn..r..
@@ -384,332 +378,6 @@ fn err_invalid_num (l: &mut Context, c: char) -> Option<Token> {
     None
 }
 
-// End new idea 2015-10-13
-
-//#[derive(Clone)]
-//pub struct Lexer<I>
-//where I: Iterator<Item=char>
-//{
-//    token: Token,               // current token
-//    input: IterLookahead<I>,    // remaining chars w/1 lookahead
-//    buf:   String,              // shared string builder
-//    start: Pos,                 // position of first char of    token
-//    end:   Pos,                 // position of first char after token
-//                                // ...also, position of self.ch()
-//    syms:  Box<SymbolTable>,
-//    errs:  u32,
-//    // symbols \__ a compilation context?
-//    // errors  /
-//}
-//
-//impl<I> Lexer<I>
-//where I: Iterator<Item=char>
-//{
-//    fn new(iter: I) -> Self {
-//        Lexer {
-//            token: Eof,
-//            input: IterLookahead::new(iter),
-//            buf:   String::with_capacity(128),
-//            start: Pos { byte: 0, line: 1, column: 1 },
-//            end:   Pos { byte: 0, line: 1, column: 1 },
-//            syms:  Box::new(SymbolTable::new()),
-//            errs:  0
-//        }
-//    }
-//
-//    fn lex(&mut self) -> Token {
-//        loop {
-//            let result = match self.ch() {
-//                None    => self.lex_eof(),
-//                Some(c) => match c {
-//                    ' ' | '\t'             => self.lex_space(),
-//                    '\r'                   => self.lex_cr(),
-//                    '\n'                   => self.lex_lf(),
-//                    '0'                    => self.lex_num_zero(),
-//                    '1'...'9'              => self.lex_num_nonzero(),
-//                    '_'                    => self.lex_id(),
-//                    _ if c.is_alphabetic() => self.lex_id(),
-//                    _                      => self.lex_other()
-//                }
-//            };
-//            if let Some(token) = result {
-//                self.token = token;
-//                return token;
-//            }
-//        }
-//    }
-//
-//    fn lex_eof(&mut self) -> Option<Token> {
-//        self.start();
-//        Some(Eof)
-//    }
-//
-//    fn lex_space(&mut self) -> Option<Token> {
-//        while let Some(c@' ') = self.advance().ch() {
-//            if c != ' ' && c != '\t' { break }
-//        }
-//        None
-//    }
-//
-//    fn lex_cr(&mut self) -> Option<Token> {
-//        if let Some(c@'\n') = self.start().newline().ch() {
-//            self.consume();
-//        }
-//        Some(Eos)
-//    }
-//
-//    fn lex_lf(&mut self) -> Option<Token> {
-//        self.start().newline();
-//        Some(Eos)
-//    }
-//
-//    // Scan identifier (at alpha or _)
-//    //
-//    fn lex_id(&mut self) -> Option<Token> {
-//        let input = self.start().ch();
-//        self.buf.push(input.unwrap());
-//
-//        while let Some(c) = self.advance().ch() {
-//            if c != '_' && !c.is_alphanumeric() { break; }
-//            self.buf.push(c);
-//        }
-//
-//        let sym = self.syms.intern(&self.buf);
-//        self.buf.clear();
-//        Some(Id(sym))
-//    }
-//
-//    // Scan number (at initial 0)
-//    //
-//    fn lex_num_zero(&mut self) -> Option<Token> {
-//        match self.start().advance().ch() {
-//            Some(c) => match c {
-//                '0'...'9' => { self          .lex_num_zero_digit (c)     },
-//                '_'       => { self.advance().lex_num_rest       (10, 0) },
-//                'x'       => { self.advance().lex_num_radix      (16   ) },
-//                'o'       => { self.advance().lex_num_radix      ( 8   ) },
-//                'b'       => { self.advance().lex_num_radix      ( 2   ) },
-//                 _        => { self          .lex_after_num      ( c, 0) }
-//            },
-//            None => Some(Int(0))
-//        }
-//    }
-//
-//    // Scan number (at initial 1-9)
-//    //
-//    #[inline]
-//    fn lex_num_nonzero(&mut self) -> Option<Token> {
-//        let val = self.ch().unwrap().to_digit(10).unwrap();
-//        self.start().advance().lex_num_rest(10, val)
-//    }
-//
-//    // Scan number (after initial 0, at digit)
-//    //
-//    #[inline]
-//    fn lex_num_zero_digit(&mut self, c: char) -> Option<Token> {
-//        self.lex_num_radix_digit(10, c.to_digit(10).unwrap())
-//    }
-//
-//    // Scan number (at first char after any radix prefix)
-//    //
-//    fn lex_num_radix(&mut self, radix: u32) -> Option<Token> {
-//        let mut input = self.ch();
-//
-//        // Ignore leading _
-//        while let Some('_') = input {
-//            input = self.advance().ch();
-//        }
-//
-//        // Require a digit
-//        if let Some(c) = input {
-//            if let Some(d) = c.to_digit(radix) {
-//                return self.advance().lex_num_rest(radix, d)
-//            }
-//        }
-//
-//        // Eof doesn't make sense here
-//        self.err_int_format()
-//    }
-//
-//    // Scan number (at first digit of known radix)
-//    //
-//    fn lex_num_radix_digit(&mut self, radix: u32, mut value: u32) -> Option<Token> {
-//        loop {
-//            let input = self.advance().ch();
-//            match input {
-//                Some('_') => {},
-//                Some( c ) => match c.to_digit(radix) {
-//                    Some(d) => value = value * radix + d,
-//                    _       => return self.lex_after_num(c, value)
-//                },
-//                _ => return Some(Int(value))
-//            }
-//        }
-//    }
-//
-//    // Scan number (after first digit of known radix)
-//    //
-//    fn lex_num_rest(&mut self, radix: u32, mut value: u32) -> Option<Token> {
-//        let mut input = self.ch();
-//
-//        // Match digits or _
-//        loop {
-//        //    while let Some('_') = input {
-//        //        input = self.advance().ch();
-//        //    }
-//
-//        //    if let Some(c) = input {
-//        //        if let Some(d) = c.to_digit(radix) {
-//        //            input = self.advance().ch();
-//        //            value = value * radix + d;
-//        //            continue;
-//        //        } else {
-//        //            return self.lex_after_num(c, value);
-//        //        }
-//        //    }
-//
-//        //    // Eof is OK here
-//        //    return Some(Int(value))
-//        //}
-//
-//            match input {
-//                Some('_') => {
-//                    input = self.advance().ch();
-//                },
-//                Some(c) => {
-//                    match c.to_digit(radix) {
-//                        Some(d) => {
-//                            input = self.advance().ch();
-//                            value = value * radix + d;
-//                        },
-//                        _ => return self.lex_after_num(c, value)
-//                    }
-//                },
-//                None => return Some(Int(value))
-//            }
-//        }
-//    }
-//
-//    // Finish scanning number (at char after num)
-//    //
-//    fn lex_after_num(&mut self, c: char, val: u32) -> Option<Token> {
-//        // OK if at valid char
-//        if !c.is_digit(10) && !c.is_alphabetic() {
-//            return Some(Int(val));
-//        }
-//
-//        // Eat invalid chars
-//        while let Some(c) = self.advance().ch() {
-//            if !c.is_digit(10) && !c.is_alphabetic() {
-//                break;
-//            }
-//        }
-//
-//        return self.err_int_format();
-//    }
-//
-//    fn lex_other(&mut self) -> Option<Token> {
-//        self.advance().ch();
-//        self.errs += 1; // TODO: error message
-//        None
-//    }
-//
-//    #[inline]
-//    fn ch(&self) -> Option<char> {
-//        self.input.current()
-//    }
-//
-//    // Consumes the current character without advancing position.
-//    #[inline]
-//    fn consume(&mut self) -> &mut Self {
-//        self.end.byte += self.ch().unwrap().len_utf8();
-//        self.input.next();
-//        self
-//    }
-//
-//    // Consumes the current character and advances position to the next column.
-//    #[inline]
-//    fn advance(&mut self) -> &mut Self {
-//        self.consume();
-//        self.end.column += 1;
-//        self
-//    }
-//
-//    // Consumes the current character and advances position to the next line.
-//    #[inline]
-//    fn newline(&mut self) -> &mut Self {
-//        self.consume();
-//        self.end.line   += 1;
-//        self.end.column  = 1;
-//        self
-//    }
-//
-//    // Sets the current position as the start of a token.
-//    #[inline]
-//    fn start(&mut self) -> &mut Self {
-//        self.start = self.end;
-//        self
-//    }
-//
-//    // Adds an error message
-//    #[inline]
-//    fn error<S>(&mut self, msg: S) -> Option<Token> where S: Into<String> {
-//        println!("{}", msg.into());
-//        None
-//    }
-//
-//    fn err_int_format(&mut self) -> Option<Token> {
-//        self.error("Invalid integer literal.")
-//    }
-//}
-//
-//impl<I> Lookahead for Lexer<I>
-//where I: Iterator<Item=char>
-//{
-//    type Item = Token;
-//
-//    #[inline]
-//    fn current(&self) -> Token {
-//        self.token
-//    }
-//
-//    #[inline]
-//    fn next(&mut self) -> Token {
-//        self.lex();
-//        self.token
-//    }
-//}
-//
-//#[derive(Clone)]
-//struct IterLookahead<I> where I: Iterator, I::Item: Copy {
-//    item: Option<I::Item>,
-//    iter: I
-//}
-//
-//impl<I> IterLookahead<I> where I: Iterator, I::Item: Copy {
-//    #[inline]
-//    fn new(mut iter: I) -> Self {
-//        let item = iter.next();
-//        IterLookahead { item: item, iter: iter }
-//    }
-//}
-//
-//impl<I> Lookahead for IterLookahead<I> where I: Iterator, I::Item: Copy {
-//    type Item = Option<I::Item>;
-//
-//    #[inline]
-//    fn current(&self) -> Self::Item {
-//        self.item
-//    }
-//
-//    #[inline]
-//    fn next(&mut self) -> Self::Item {
-//        let item = self.iter.next();
-//        self.item = item;
-//        item
-//    }
-//}
-//
 //#[cfg(test)]
 //mod tests {
 //    use super::*;
