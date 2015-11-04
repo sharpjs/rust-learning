e12b83d3f449788b2c16fb55125857c43f8a00b7
Jeff Sharp <jeff.sharp@git>
2015-11-04 09:22:12 -0600

    Simplify static messages array so some macros are not needed.

--- src/message.rs
+++ src/message.rs
@@ -18,28 +18,21 @@
 
 // Defines a messages enum and a map from enum items to strings.
 macro_rules! messages {
-    ( $( $id:ident => $str:expr ),* ) => (
+    { $( $id:ident => $str:expr ),* } => {
         #[allow(non_camel_case_types)]
         #[derive(Clone, Copy, PartialEq, Eq, Debug)]
         #[repr(u8)]
-        pub enum Message { $( $id ),* }
+        pub enum Message {
+            $( $id ),*
+        }
 
-        static_array! { MESSAGES: [&'static str] = $( $str ),* }
-    );
+        const MESSAGES: &'static [&'static str] = &[
+            $( $str ),*
+        ];
+    };
 }
 
-// Defines a static array with automatic count.
-macro_rules! static_array {
-    [ $id:ident: [$t:ty] = $( $e:expr ),* ] => (
-        static $id: [$t; count!($( $e ),*)] = [$( $e ),*];
-    );
-}
 
-// Counts arguments.  Yields an expression like: 1 + 1 + 1
-macro_rules! count {
-    ( $e:expr, $( $x:tt )+ ) => (1 + count!($( $x )+));
-    ( $e:expr              ) => (1);
-    (                      ) => (0);
 }
 
 messages! {
