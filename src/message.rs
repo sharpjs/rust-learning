c0730e08f3a3f00c5de7416ff413cbd82b281c79
Jeff Sharp <jeff.sharp@git>
2015-11-06 06:33:00 -0600

    Add Messages collection

--- src/message.rs
+++ src/message.rs
@@ -17,11 +17,44 @@
 // along with AEx.  If not, see <http://www.gnu.org/licenses/>.
 
 use util::Pos;
-use std::borrow::Cow;
+use std::borrow::{Cow};
+use std::fmt::{self, Display};
 use std::io::{stderr, Write};
 
+#[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Debug)]
+#[repr(u8)]
+pub enum MessageLevel { Warning, Error }
+use self::MessageLevel::*;
+
+#[allow(non_camel_case_types)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[repr(u8)]
+pub enum MessageId {
+    // Lexer Messages
+    Lex_Invalid,
+    Lex_NumInvalid,
+    Lex_NumOverflow,
+    Lex_RawUnterminated,
+    Lex_StrUnterminated,
+    Lex_CharUnterminated,
+    Lex_CharLength,
+    Lex_EscInvalid,
+    Lex_EscUnterminated,
+    Lex_EscOverflow,
+    // Parser Messages
+}
+use self::MessageId::*;
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub struct Message {
+    pub position:   Pos,
+    pub level:      MessageLevel,
+    pub id:         MessageId,
+    pub text:       Cow<'static, str>,
+}
+
 pub struct Messages {
-    messages:    Vec<Message_>,
+    messages:    Vec<Message>,
     error_count: usize
 }
 
@@ -42,83 +75,116 @@ impl Messages {
     }
 
     pub fn print(&self) {
-        for m in &self.messages {
-            let r = writeln!(stderr(), "{}:{}: {:?}: {}",
-                m.position.line,
-                m.position.column,
-                m.level,
-                m.text
-            );
+        let r = write!(stderr(), "{}", self);
+        if let Err(_) = r { /*ignore*/ }
+    }
 
-            if let Err(_) = r { return }
+    fn add<T>(&mut self, p: Pos, l: MessageLevel, i: MessageId, t: T)
+    where T: Into<Cow<'static, str>> {
+        self.messages.push(Message {
+            position: p, level: l, id: i, text: t.into()
+        });
+        if l >= Error {
+            self.error_count += 1;
         }
+    }
+
+    pub fn err_invalid_char(&mut self, p: Pos, c: char) {
+        self.add(p, Error, Lex_Invalid, format!(
+            "Unrecognized character: '{}'", c
+        ));
+    }
+
+    pub fn err_num_invalid_char(&mut self, p: Pos, c: char) {
+        self.add(p, Error, Lex_NumInvalid, format!(
+            "Unrecognized character in number literal: '{}'", c
+        ));
+    }
+
+    pub fn err_num_overflow(&mut self, p: Pos) {
+        self.add(p, Error, Lex_NumOverflow,
+            "Overflow in number literal.  Integers are unsigned 64-bit."
+        );
+    }
+
+//    Lex_RawUnterminated  => "Unterminated raw block.",
+//    Lex_StrUnterminated  => "Unterminated string literal.",
+//    Lex_CharUnterminated => "Unterminated character literal.",
+//    Lex_CharLength       => "Invalid character literal length.  \
+//                             Character literals must contain exactly one character.",
+//    Lex_EscInvalid       => "Overflow in Unicode escape sequence.  \
+//                             The maximum permitted is \\u{10FFFF}.",
+//    Lex_EscUnterminated  => "Incomplete escape sequence.",
+//    Lex_EscOverflow      => "Invalid escape sequence."
+}
+
+impl Display for Messages {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for m in &self.messages {
+            try!(writeln!(f, "{}", m));
+        }
+        Ok(())
     }
 }
 
-fn format_message(m: &Message_) -> &str { &m.text }
-
-pub struct Message_ {
-    pub id:         MessageId,
-    pub level:      MessageLevel,
-    pub position:   Pos,
-    pub text:       Cow<'static, str>,
+impl Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}:{}: {}{:03}: {}",
+            "(file)",
+            self.position.line,
+            self.position.column,
+            match self.level { Warning => 'W', Error => 'E' },
+            self.id as u16,
+            self.text
+        )
+    }
 }
 
-impl Message_ {
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use util::Pos;
 
-pub type MessageId = Message;
+    #[test]
+    fn messages_empty() {
+        let m = Messages::new();
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum MessageLevel { Warning, Error }
+        assert_eq!(0,     m.error_count());
+        assert_eq!(false, m.has_errors());
+        assert_eq!("",    m.to_string());
+    }
 
-// Defines a messages enum and a map from enum items to strings.
-macro_rules! messages {
-    { $( $id:ident => $str:expr ),* } => {
-        #[allow(non_camel_case_types)]
-        #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-        #[repr(u8)]
-        pub enum Message {
-            $( $id ),*
-        }
+    #[test]
+    fn messages_single() {
+        let mut m = Messages::new();
+        let     p = Pos::bof();
 
-        const MESSAGES: &'static [&'static str] = &[
-            $( $str ),*
-        ];
-    };
-}
+        m.err_invalid_char(p, 'c');
 
-messages! {
-    Lex_Invalid          => "Unrecognized character.",
-    Lex_NumInvalid       => "Invalid character in numeric literal.",
-    Lex_NumOverflow      => "Overflow in numeric literal.  \
-                             Aex integers are unsigned 64-bit.",
-    Lex_RawUnterminated  => "Unterminated raw block.",
-    Lex_StrUnterminated  => "Unterminated string literal.",
-    Lex_CharUnterminated => "Unterminated character literal.",
-    Lex_CharLength       => "Invalid character literal length.  \
-                             Character literals must contain exactly one character.",
-    Lex_EscInvalid       => "Overflow in Unicode escape sequence.  \
-                             The maximum permitted is \\u{10FFFF}.",
-    Lex_EscUnterminated  => "Incomplete escape sequence.",
-    Lex_EscOverflow      => "Invalid escape sequence."
-}
+        assert_eq!(1,     m.error_count());
+        assert_eq!(true,  m.has_errors());
+        assert_eq!(
+            "(file):1:1: E000: Unrecognized character: 'c'\n",
+            m.to_string()
+        );
+    }
 
-impl Message {
-    #[inline]
-    fn number(self) -> u8 { self as u8 }
+    #[test]
+    fn messages_multiple() {
+        let mut m = Messages::new();
+        let mut p = Pos::bof();
 
-    #[inline]
-    fn text(self) -> &'static str { MESSAGES[self as usize] }
-}
+        m.err_invalid_char(p, 'c');
+        p.advance('c');
+        m.err_invalid_char(p, 'd');
 
-impl Into<u8> for Message {
-    #[inline]
-    fn into(self) -> u8 { self.number() }
-}
-
-impl Into<&'static str> for Message {
-    #[inline]
-    fn into(self) -> &'static str { self.text() }
+        assert_eq!(2,    m.error_count());
+        assert_eq!(true, m.has_errors());
+        assert_eq!(
+            "(file):1:1: E000: Unrecognized character: 'c'\n\
+             (file):1:2: E000: Unrecognized character: 'd'\n",
+            m.to_string()
+        );
+    }
 }
 
