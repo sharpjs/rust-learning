6126138e0049447188ea33e2b7f981b3929b3390
Jeff Sharp <jeff.sharp@git>
2016-01-03 06:28:27 -0600

    Struct to encapsulate output (code + messages).

--- src/aex/output.rs
+++ src/aex/output.rs
@@ -1,7 +1,7 @@
-// Aex Root Module
+// Compilation Output
 //
 // This file is part of AEx.
-// Copyright (C) 2015 Jeffrey Sharp
+// Copyright (C) 2016 Jeffrey Sharp
 //
 // AEx is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published
@@ -16,18 +16,20 @@
 // You should have received a copy of the GNU General Public License
 // along with AEx.  If not, see <http://www.gnu.org/licenses/>.
 
-mod analyze;
-mod ast;
-mod asm;
-mod mem;
-mod message;
-mod output;
-mod pos;
-mod scope;
-mod symbol;
-mod targets;
-mod types;
+use aex::asm::Assembly;
+use aex::message::Messages;
 
-//mod lexer;
-//mod parser;
+pub struct Output<'str> {
+    pub asm: Assembly,
+    pub log: Messages<'str>,
+}
+
+impl<'str> Output<'str> {
+    pub fn new() -> Self {
+        Output {
+            asm: Assembly::new(),
+            log: Messages::new(),
+        }
+    }
+}
 
