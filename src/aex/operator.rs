ce89d50b131bc4745d988ee02a19a46c6046445c
Jeff Sharp <jeff.sharp@git>
2016-03-30 06:55:38 -0500

    Separate maps for prefix and nonprefix operators.  Add all operators.

--- src/aex/operator.rs
+++ src/aex/operator.rs
@@ -19,7 +19,7 @@
 // FUTURE:
 //   - Use operator table
 //   - Targets add their own operators at startup
-//   - Token::Operator(str)
+//   - Token::Op      (op)
 //   - Expr::BinaryOp (op, lhs, rhs, sel)
 //   - Expr::UnaryOp  (op, expr,     sel)
 
@@ -29,12 +29,13 @@ use self::Assoc::*;
 use self::Fix::*;
 
 #[derive(Clone, Debug)]
-pub struct OperatorTable (
-    HashMap<&'static str, Operator>
-);
+pub struct OpTable {
+    nonprefix: HashMap<&'static str, Op>, // infix and postfix ops
+    prefix:    HashMap<&'static str, Op>, // prefix ops
+}
 
 #[derive(Clone, Copy, Debug)]
-pub struct Operator {
+pub struct Op {
     pub chars: &'static str,
     pub prec:  u8,
     pub assoc: Assoc,
@@ -48,30 +49,84 @@ pub enum Assoc { Left, Right }
 #[derive(Clone, Copy, Debug)]
 pub enum Fix { Prefix, Infix, Postfix }
 
-impl OperatorTable {
+impl OpTable {
     pub fn new() -> Self {
-        OperatorTable(HashMap::new())
+        OpTable { 
+            nonprefix: HashMap::new(),
+            prefix:    HashMap::new(),
+        }
     }
 
-    pub fn add(&mut self, op: Operator) {
-        self.0.insert(op.chars, op);
+    pub fn add(&mut self, op: Op) {
+        let map = match op.fix {
+            Infix | Postfix => &mut self.nonprefix,
+            Prefix          => &mut self.prefix,
+        };
+        map.insert(op.chars, op);
     }
 
-    pub fn get(&self, chars: &str) -> Option<&Operator> {
-        self.0.get(chars)
+    pub fn get_nonprefix(&self, chars: &str) -> Option<&Op> {
+        self.nonprefix.get(chars)
+    }
+
+    pub fn get_prefix(&self, chars: &str) -> Option<&Op> {
+        self.prefix.get(chars)
     }
 }
 
-fn create_op_table() -> OperatorTable {
-    let mut table = OperatorTable::new();
-    table.add(EQUAL);
+fn create_op_table() -> OpTable {
+    let mut table = OpTable::new();
+    for &op in OPS { table.add(op) }
     table
 }
 
-static EQUAL: Operator = Operator {
-    chars: "=",
-    prec:  0,
-    assoc: Right,
-    fix:   Infix,
-};
+static OPS: &'static [Op] = &[
+    // Postfix Unary
+    Op { chars: "++", prec: 10, assoc: Left,  fix: Postfix },
+    Op { chars: "--", prec: 10, assoc: Left,  fix: Postfix },
+
+    // Prefix Unary
+    Op { chars: "!" , prec:  9, assoc: Right, fix: Prefix  },
+    Op { chars: "~" , prec:  9, assoc: Right, fix: Prefix  },
+    Op { chars: "-" , prec:  9, assoc: Right, fix: Prefix  },
+    Op { chars: "+" , prec:  9, assoc: Right, fix: Prefix  },
+    Op { chars: "&" , prec:  9, assoc: Right, fix: Prefix  },
+
+    // Multiplicative
+    Op { chars: "*" , prec:  8, assoc: Left,  fix: Infix   },
+    Op { chars: "/" , prec:  8, assoc: Left,  fix: Infix   },
+    Op { chars: "%" , prec:  8, assoc: Left,  fix: Infix   },
+
+    // Additive
+    Op { chars: "+" , prec:  7, assoc: Left,  fix: Infix   },
+    Op { chars: "-" , prec:  7, assoc: Left,  fix: Infix   },
+
+    // Bitwise Shift
+    Op { chars: "<<", prec:  6, assoc: Left,  fix: Infix   },
+    Op { chars: ">>", prec:  6, assoc: Left,  fix: Infix   },
+
+    // Bitwise Boolean
+    Op { chars: "&" , prec:  5, assoc: Left,  fix: Infix   },
+    Op { chars: "^" , prec:  4, assoc: Left,  fix: Infix   },
+    Op { chars: "|" , prec:  3, assoc: Left,  fix: Infix   },
+
+    // Bitwise Manipulation
+    Op { chars: ".~", prec:  2, assoc: Left,  fix: Infix   },
+    Op { chars: ".!", prec:  2, assoc: Left,  fix: Infix   },
+    Op { chars: ".+", prec:  2, assoc: Left,  fix: Infix   },
+    Op { chars: ".?", prec:  2, assoc: Left,  fix: Infix   },
+
+    // Comparison
+    Op { chars: "?" , prec:  1, assoc: Left,  fix: Postfix },
+    Op { chars: "<>", prec:  1, assoc: Left,  fix: Infix   },
+    Op { chars: "==", prec:  1, assoc: Left,  fix: Infix   },
+    Op { chars: "!=", prec:  1, assoc: Left,  fix: Infix   },
+    Op { chars: "<" , prec:  1, assoc: Left,  fix: Infix   },
+    Op { chars: "<=", prec:  1, assoc: Left,  fix: Infix   },
+    Op { chars: ">" , prec:  1, assoc: Left,  fix: Infix   },
+    Op { chars: ">=", prec:  1, assoc: Left,  fix: Infix   },
+
+    // Assignment
+    Op { chars: "=",  prec:  0, assoc: Right, fix: Infix   },
+];
 
