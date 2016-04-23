35b57be591b5a33c0acd75cab27e2505d4e75f48
Jeff Sharp <jeff.sharp@git>
2016-04-23 10:07:03 -0500

    Cosmetic

--- src/aex/types/builtin.rs
+++ src/aex/types/builtin.rs
@@ -47,23 +47,23 @@ macro_rules! float {
 
 types! {
     // Abstract
-    INT:    int!    ();
-    FLOAT:  float!  ();
+    INT:   int!   ();
+    FLOAT: float! ();
 
     // Concrete unsigned integer
-     U8:    int!    ( 8,  8, false);
-    U16:    int!    (16, 16, false);
-    U32:    int!    (32, 32, false);
-    U64:    int!    (64, 64, false);
+     U8: int! ( 8,  8, false);
+    U16: int! (16, 16, false);
+    U32: int! (32, 32, false);
+    U64: int! (64, 64, false);
 
     // Concrete signed integer
-     I8:    int!    ( 8,  8, true);
-    I16:    int!    (16, 16, true);
-    I32:    int!    (32, 32, true);
-    I64:    int!    (64, 64, true);
+     I8: int! ( 8,  8, true);
+    I16: int! (16, 16, true);
+    I32: int! (32, 32, true);
+    I64: int! (64, 64, true);
 
     // Concrete floating-point
-    F32:    float!  (32, 32);
-    F64:    float!  (64, 64);
+    F32: float! (32, 32);
+    F64: float! (64, 64);
 }
 
