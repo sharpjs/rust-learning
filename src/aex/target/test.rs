2676c0f412a4e7994f9195a5294e32b52683f25c
Jeff Sharp <jeff.sharp@git>
2016-07-16 07:58:21 -0500

    Flexh out ideas in TestTarget a little.

--- src/aex/target/test.rs
+++ src/aex/target/test.rs
@@ -16,18 +16,55 @@
 // You should have received a copy of the GNU General Public License
 // along with AEx.  If not, see <http://www.gnu.org/licenses/>.
 
+use std::fmt::{self, Debug, Display, Formatter};
+use aex::operator::OperatorTable;
+use aex::scope::Scope;
 use aex::target::Target;
 
-#[derive(Debug)]
-pub struct TestTarget;
+pub struct TestTarget (BaseTarget);
 
 impl TestTarget {
-    pub fn new() -> Self { TestTarget }
+    pub fn new() -> Self {
+        let mut target = TestTarget(BaseTarget::new());
+        target.init();
+        target
+    }
+
+    fn init(&mut self) {
+    }
 }
 
 impl Target for TestTarget {
     // ...
 }
 
+impl<'a> Display for TestTarget {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        f.write_str("test")
+    }
+}
+
+impl<'a> Debug for TestTarget {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        f.write_str("TestTarget")
+    }
+}
+
 pub type TestValue = ();
+
+// -----------------------------------------------------------------------------
+
+pub struct BaseTarget {
+    pub operators:  OperatorTable,
+    pub root_scope: Scope<'static>,
+}
+
+impl BaseTarget {
+    pub fn new() -> Self {
+        BaseTarget {
+            operators:  OperatorTable::new(),
+            root_scope: Scope::new(),
+        }
+    }
+}
 
