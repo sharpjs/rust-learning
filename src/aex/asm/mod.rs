2884dca37f1b5629124ad22155618e7de1a56b6d
Jeff Sharp <jeff.sharp@git>
2017-03-11 17:29:44 -0800

    AsmDisplay trait, now supporting generic context in AST.

--- src/aex/asm/mod.rs
+++ src/aex/asm/mod.rs
@@ -16,25 +16,27 @@
 // You should have received a copy of the GNU General Public License
 // along with AEx.  If not, see <http://www.gnu.org/licenses/>.
 
-//use std::fmt::{self, Debug, Display, Formatter};
-
 //pub use self::att::*;
 //pub use self::intel::*;
 //pub use self::mit::*;
+use std::fmt::{self, Debug, Display, Formatter};
+use aex::ast::*;
 
 //pub mod att;
 //pub mod intel;
 //pub mod mit;
 
 
-/*
 // -----------------------------------------------------------------------------
 
-pub trait AsmDisplay {
-    fn fmt(&self, f: &mut Formatter, s: &AsmStyle) -> fmt::Result;
+/// A type that is formattable as assembly code.
+pub trait AsmDisplay<C = ()> {
+    /// Formats the value as assembly code, using the given formatter and
+    /// assembly style.
+    fn fmt(&self, f: &mut Formatter, s: &AsmStyle<C>) -> fmt::Result;
 }
 
-impl<'a, T> Display for Asm<'a, T> where T: AsmDisplay + ?Sized {
+impl<'a, T, C> Display for Asm<'a, T, C> where T: AsmDisplay<C> + ?Sized {
     #[inline]
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         let Asm(value, style) = *self;
@@ -44,19 +46,24 @@ impl<'a, T> Display for Asm<'a, T> where T: AsmDisplay + ?Sized {
 
 // -----------------------------------------------------------------------------
 
+/// An assembly-formattable value paired with an assembly style.
 #[derive(Clone, Copy, Debug)]
-pub struct Asm<'a, T: 'a + ?Sized>(
+pub struct Asm<'a, T: 'a + AsmDisplay<C> + ?Sized, C: 'a = ()>(
+    /// Assembly-formattable value.
     pub &'a T,
-    pub &'a AsmStyle
+    /// Assembly code style.
+    pub &'a AsmStyle<C>
 );
 
 // -----------------------------------------------------------------------------
 
-pub trait AsmStyle : Debug {
-    fn write_id<'a>(&self, f: &mut Formatter, value: &Id<'a>) -> fmt::Result {
-        Ok(())
+/// An assembly code style.
+pub trait AsmStyle<C = ()> : Debug {
+    /// Writes an identifier to the given formatter in this assembly style.
+    fn write_id(&self, f: &mut Formatter, id: &Id<C>) -> fmt::Result {
+        f.write_str(id.name)
     }
-
+/*
     fn write_int(&self, f: &mut Formatter, value: u64) -> fmt::Result {
         write!(f, "{}", value)
     }
@@ -88,7 +95,10 @@ pub trait AsmStyle : Debug {
     fn write_scale
         (&self, f: &mut Formatter, scale: u8)
         -> fmt::Result { Err(fmt::Error) }
+*/
 }
+
+/*
 
 #[cfg(test)]
 pub static GAS_STYLE: AttStyle = AttStyle {
