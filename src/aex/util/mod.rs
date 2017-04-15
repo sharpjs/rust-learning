dc427d8378f4769d8fd1acfb613136eb1e7dd809
Jeff Sharp <jeff.sharp@git>
2017-04-15 06:34:39 -0500

    Remove trie exploration. Going to go a different direction for decoding.

--- src/aex/util/mod.rs
+++ src/aex/util/mod.rs
@@ -16,8 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with AEx.  If not, see <http://www.gnu.org/licenses/>.
 
-pub mod decode_trie;
-
 /*
 use std::io::{self, Error, ErrorKind};
 
