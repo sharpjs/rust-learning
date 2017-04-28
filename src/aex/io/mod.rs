2d377fbce3a339450edbc47b96e710d78be20009
Jeff Sharp <jeff.sharp@git>
2017-04-28 06:31:37 -0500

    WIP on a ReadAhead impl over BufRead

--- src/aex/io/mod.rs
+++ src/aex/io/mod.rs
@@ -16,6 +16,9 @@
 // You should have received a copy of the GNU General Public License
 // along with AEx.  If not, see <http://www.gnu.org/licenses/>.
 
+use std::io::{/*self,*/ BufRead, Cursor};
+use std::ops::Range;
+
 /// A forward-only, read-only cursor with rewindable read-ahead.
 ///
 /// Read bytes remain unconsumed until `consume` is called, upon which the
@@ -59,5 +62,58 @@ pub trait ReadAhead {
     /// * `len` becomes `0`.
     ///
     fn rewind(&mut self);
+}
+
+#[derive(Debug)]
+pub struct BufReadAhead<'a, R: BufRead + 'a> {
+    more:  Cursor<&'a [u8]>,    // current read-ahead
+    src:   &'a mut R,           // source stream
+    start: u64,                 // byte position of first read-ahead byte
+}
+
+static EMPTY: [u8; 0] = [];
+
+impl<'a, R: BufRead> BufReadAhead<'a, R> {
+    pub fn new(src: &'a mut R, pos: u64) -> Self {
+        Self {
+            more:  Cursor::new(&EMPTY),
+            src:   src,
+            start: pos,
+        }
+    }
+}
+
+impl<'a, R: BufRead> ReadAhead for BufReadAhead<'a, R> {
+    /// Returns the position of the first byte in the current read-ahead.
+    #[inline]
+    fn start(&self) -> u64 { self.start }
+
+    /// Returns the count of bytes in the current read-ahead.
+    #[inline]
+    fn len(&self) -> u64 { self.more.position() }
+
+ // #[inline]
+ // fn get(&mut self, range: Range<usize>) -> &[u8] {
+ //   //self.len = cmp::max(self.len, range.end);
+ //     &self.more.get_ref()[range]
+ // }
+
+    /// Consumes the current read-ahead and advances the cursor to the first
+    /// unread byte.
+    ///
+    /// * `start` advances by `len` and becomes equal to `end`.
+    /// * `end` remains unchanged.
+    /// * `len` becomes `0`.
+    ///
+    fn consume(&mut self) { panic!() }
+
+    /// Forgets the current read-ahead and rewinds the cursor to the first
+    /// unconsumed byte.
+    ///
+    /// * `start` remains unchanged.
+    /// * `end` rewinds by `len` and becomes equal to `start`.
+    /// * `len` becomes `0`.
+    ///
+    fn rewind(&mut self) { panic!() }
 }
 
