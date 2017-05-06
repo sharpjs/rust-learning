796194c2a5acd70a36f5fe5ec17c4f23184c86fb
Jeff Sharp <jeff.sharp@git>
2017-05-06 06:28:24 -0500

    Tests for DecodeReader

--- src/aex/io/decode_read.rs
+++ src/aex/io/decode_read.rs
@@ -208,3 +208,167 @@ impl<R: RewindRead> DecodeRead for DecodeReader<R> {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+    use aex::util::{BE, LE};
+    use super::super::RewindReader;
+    use super::*;
+
+    #[test]
+    fn lma() {
+        let mut r = reader();
+        assert_eq!(r.lma(), 0, "initial");
+        r.read_bytes(2).unwrap();
+        assert_eq!(r.lma(), 0, "after read");
+        r.consume();
+        assert_eq!(r.lma(), 2, "after consume");
+    }
+
+    #[test]
+    fn vma() {
+        let mut r = reader();
+        assert_eq!(r.vma(), 0x100, "initial");
+        r.read_bytes(2).unwrap();
+        assert_eq!(r.vma(), 0x100, "after read");
+        r.consume();
+        assert_eq!(r.vma(), 0x102, "after consume");
+        r.set_reloc(0x400);
+        assert_eq!(r.vma(), 0x402, "after relocation change");
+    }
+
+    #[test]
+    fn reloc() {
+        let mut r = reader();
+        assert_eq!(r.reloc(), 0x100);
+        r.set_reloc(0x400);
+        assert_eq!(r.reloc(), 0x400);
+    }
+
+    #[test]
+    fn byte_order() {
+        let mut r = reader();
+        assert_eq!(r.byte_order(), BE);
+        r.set_byte_order(LE);
+        assert_eq!(r.byte_order(), LE);
+    }
+
+    #[test]
+    fn read_bytes() {
+        let mut r = reader();
+        assert_eq!(r.read_bytes(2).unwrap(), &[0x12, 0x34]);
+
+        // read_bytes() should be byte-order agnostic
+        let mut r = reader();
+        r.set_byte_order(LE);
+        assert_eq!(r.read_bytes(2).unwrap(), &[0x12, 0x34]);
+    }
+
+    #[test]
+    fn read_u8() {
+        let mut r = reader();
+        assert_eq!(r.read_u8().unwrap(), 0x12);
+
+        // read_u8() should be byte-order agnostic
+        let mut r = reader();
+        r.set_byte_order(LE);
+        assert_eq!(r.read_u8().unwrap(), 0x12);
+    }
+
+    #[test]
+    fn read_i8() {
+        let mut r = reader();
+        assert_eq!(r.read_i8().unwrap(), 0x12);
+
+        // read_i8() should be byte-order agnostic
+        let mut r = reader();
+        r.set_byte_order(LE);
+        assert_eq!(r.read_i8().unwrap(), 0x12);
+    }
+
+    #[test]
+    fn read_u16_be() {
+        let mut r = reader();
+        assert_eq!(r.read_u16().unwrap(), 0x1234);
+    }
+
+    #[test]
+    fn read_u16_le() {
+        let mut r = reader();
+        r.set_byte_order(LE);
+        assert_eq!(r.read_u16().unwrap(), 0x3412);
+    }
+
+    #[test]
+    fn read_i16_be() {
+        let mut r = reader();
+        assert_eq!(r.read_i16().unwrap(), 0x1234);
+    }
+
+    #[test]
+    fn read_i16_le() {
+        let mut r = reader();
+        r.set_byte_order(LE);
+        assert_eq!(r.read_i16().unwrap(), 0x3412);
+    }
+
+    #[test]
+    fn read_u32_be() {
+        let mut r = reader();
+        assert_eq!(r.read_u32().unwrap(), 0x12345678);
+    }
+
+    #[test]
+    fn read_u32_le() {
+        let mut r = reader();
+        r.set_byte_order(LE);
+        assert_eq!(r.read_u32().unwrap(), 0x78563412);
+    }
+
+    #[test]
+    fn read_i32_be() {
+        let mut r = reader();
+        assert_eq!(r.read_i32().unwrap(), 0x12345678);
+    }
+
+    #[test]
+    fn read_i32_le() {
+        let mut r = reader();
+        r.set_byte_order(LE);
+        assert_eq!(r.read_i32().unwrap(), 0x78563412);
+    }
+
+    #[test]
+    fn read_u64_be() {
+        let mut r = reader();
+        assert_eq!(r.read_u64().unwrap(), 0x123456789ABCDEF0);
+    }
+
+    #[test]
+    fn read_u64_le() {
+        let mut r = reader();
+        r.set_byte_order(LE);
+        assert_eq!(r.read_u64().unwrap(), 0xF0DEBC9A78563412);
+    }
+
+    #[test]
+    fn read_i64_be() {
+        let mut r = reader();
+        assert_eq!(r.read_i64().unwrap(), 0x123456789ABCDEF0);
+    }
+
+    #[test]
+    fn read_i64_le() {
+        let mut r = reader();
+        r.set_byte_order(LE);
+        assert_eq!(r.read_i64().unwrap(), -0x0F21436587A9CBEE);
+    }
+
+    fn reader() -> Box<DecodeRead> {
+        let src = vec![0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0];
+        let src = Cursor::new(src);
+        let src = RewindReader::new(src);
+        Box::new(DecodeReader::new(src, BE, 0x100))
+    }
+}
+
